9afea4bc62636fdf3c99e24285c8d649fe0d0988
beethika10 <50477791+beethika10@users.noreply.github.com>
2024-07-16 23:42:18 +0530

    Update provisioner-local-exec.tf

--- modules/packer_module/provisioner-local-exec.tf
+++ modules/packer_module/provisioner-local-exec.tf
@@ -1,26 +1,26 @@
 
 resource "terraform_data" "packer_template" {
 
-//provisioner "local-exec" {
-//command = "mkdir -p /opt/packer"
-//}
-//provisioner "local-exec" {
-//working_dir = "/opt/packer/"
-//command = "wget https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_amd64.zip"
-//}
-//provisioner "local-exec" {
-//command = "ls -al /opt/packer"
-//}
-//provisioner "local-exec" {
-//command = "apt install -y unzip"
-//}
-//provisioner "local-exec" {
-//working_dir = "/opt/packer/"
-//command = "unzip -o packer_1.11.0_linux_amd64.zip"
-//}
-//provisioner "local-exec" {
-//command = "/opt/packer/packer plugins install github.com/hashicorp/amazon"
-//}
+provisioner "local-exec" {
+command = "mkdir -p /opt/packer"
+}
+provisioner "local-exec" {
+working_dir = "/opt/packer/"
+command = "wget https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_amd64.zip"
+}
+provisioner "local-exec" {
+command = "ls -al /opt/packer"
+}
+provisioner "local-exec" {
+command = "apt install -y unzip"
+}
+provisioner "local-exec" {
+working_dir = "/opt/packer/"
+command = "unzip -o packer_1.11.0_linux_amd64.zip"
+}
+provisioner "local-exec" {
+command = "/opt/packer/packer plugins install github.com/hashicorp/amazon"
+}
 provisioner "local-exec" {
    
     working_dir = "${path.module}/packer/"
