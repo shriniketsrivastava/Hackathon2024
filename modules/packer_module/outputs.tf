2f2529d2feb9fa8d30d048c418e1c817a1bdd5cb
beethika10 <50477791+beethika10@users.noreply.github.com>
2024-07-17 20:48:39 +0530

    Update outputs.tf

--- modules/packer_module/outputs.tf
+++ modules/packer_module/outputs.tf
@@ -1,7 +1,3 @@
-data "local_file" "image_version" {
-  filename = "${path.module}/packer/.image_version"
-  depends_on = [ terraform_data.packer_template ]
-}
 output "image_name" {
   value = data.local_file.image_version.content
 }
