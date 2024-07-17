df793c3432626adb2bd224729ac7b20edf404edc
Pandey <beethika.pandey@cytiva.com>
2024-07-17 23:29:14 +0530

    for packer

--- modules/packer_module/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "image_name" {
-  value = data.local_file.image_version.content
-}
-
--- variables.tf
+++ variables.tf
@@ -2,13 +2,3 @@ variable "aminame" {
   type    = string
   
 }
-
-variable "base_ami" {
-  type    = string
-  
-}
-
-variable "triggered_user" {
-  type    = string
-
-}
