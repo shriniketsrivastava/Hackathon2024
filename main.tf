e3e16d13d2010fb0a3438f80f15ea61b2670b32d
beethika10 <50477791+beethika10@users.noreply.github.com>
2024-07-17 23:56:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "aws" {
 
 data "aws_ami" "ami" {
   most_recent = true
-  owners = ["980377841215"]
+  owners = ["099720109477"]
   filter {
     name = "name"
     values = [var.aminame]
