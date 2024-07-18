b506110111bfa89f0088f6678ee07c08ea9b8cb6
shriniketsrivastava <60386619+shriniketsrivastava@users.noreply.github.com>
2024-07-18 22:04:21 +0530

    Update locals.tf

--- AMI/modules/packer_module/locals.tf
+++ AMI/modules/packer_module/locals.tf
@@ -3,5 +3,5 @@ locals {
   # Let's calculate the hash of all the contents of the packer directory.
   # We'll use this hash to determine if anything changed in the packer directory.
   packer_contents_hash        = md5(join(",", [for f in fileset("${path.module}/packer", "**") : filemd5("${path.module}/packer/${f}") if f != ".manifest.json"]))
-
+  image_target_build_params   = "-var aminame=${var.aminame}"
 }
